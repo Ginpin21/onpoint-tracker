d609316eb06b6bad5435129e59f575d7a4e79130
Iftekhar Ahmed <iftekharahmed2003@gmail.com>
2023-11-08 00:34:04 +0400

    Added head and main script file in inc folder

--- inc/head.php
+++ inc/head.php
@@ -1,3 +1,4 @@
 <head>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="inc\main.js"></script>
 </head>
---
+++ pages/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    require_once('..\inc\head.php');
+    ?>
+</head>
+<body>
+    <h1 class="text-2xl text-blue-500 font-bold">Onpoint Tracker</h1>
+</body>
+</html>
