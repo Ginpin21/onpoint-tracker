ee8b550f819172ba08924b3828411697e4658202
KARAN-SINGH19 <115944579+KARAN-SINGH19@users.noreply.github.com>
2023-11-21 00:29:26 +0400

    removed the replicating navbar

--- pages/index.php
+++ pages/index.php
@@ -36,38 +36,6 @@
 
 <body>
     <?php require_once('..\inc\navbar.php'); ?>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="index.php">Onpoint Tracker</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item <?php echo basename($_SERVER['PHP_SELF']) == 'index.php' ? 'active' : ''; ?>">
-                    <a class="nav-link" href="index.php">Home</a>
-                </li>
-                <li class="nav-item <?php echo basename($_SERVER['PHP_SELF']) == 'teacher_dashboard.php' ? 'active' : ''; ?>">
-                    <a class="nav-link" href="teacher_dashboard.php">Teacher Dashboard</a>
-                </li>
-                <li class="nav-item <?php echo basename($_SERVER['PHP_SELF']) == 'login_page.php' ? 'active' : ''; ?>">
-                    <a class="nav-link" href="login_page.php">Login</a>
-                </li>
-            </ul>
-        </div>
-        <div class="d-flex align-items-center gap-3">
-            <span class="navbar-text" style="color: #003DB2; font-weight: bold;">
-                <?php
-                $currentUserFullName = "Iftakhar Ahmed";
-                echo $currentUserFullName; // Replace this with the user's full name 
-                ?>
-            </span>
-            <form class="form-inline">
-                <button class="btn btn-danger" type="submit">Logout</button>
-            </form>
-        </div>
-    </nav>
-
-
     <div class="container">
         <div class="headings">
             <h1>Welcome to Onpoint Tracker</h1>
