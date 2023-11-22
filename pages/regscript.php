40e1ac68d64e9be204dbbb2195bfb6e59e9d0bd6
KARAN-SINGH19 <115944579+KARAN-SINGH19@users.noreply.github.com>
2023-11-23 00:43:38 +0400

    Update regscript.php

--- pages/regscript.php
+++ pages/regscript.php
@@ -3,25 +3,20 @@ session_start(); // it will start the session
 require('../inc/connection.php'); // it will include the con.php file
 if (isset($_POST['Register'])) { // the script will execute once the button is clicked
 
-    $fname = $_POST['firstName']; 
-    $lname = $_POST['lastName']; 
+    $fname = $_POST['firstName'];
+    $lname = $_POST['lastName'];
     $email = $_POST['email'];
-    $pass = $_POST['password']; 
+    $pass = $_POST['password'];
     $role_id = $_POST['role_id'];
 
     $sql = "INSERT into user (user_fname, user_lname, user_email, user_password, user_role_id) VALUES ('$fname', '$lname', '$email', '$pass', '$role_id')";
 
     $result = mysqli_query($conn, $sql); // will execute the query
-    $final = mysqli_num_rows($result); // will check whether the table is empty or not
 
-    if ($final > 0) {
-        while ($row = mysqli_fetch_array($result)) { // will fetch the data from the table
-            $_SESSION['loggedin'] = TRUE;
-            $_SESSION['name'] = $row['user_fname']; 
-            $_SESSION['email'] = $row['user_email'];
-            header("Location:index.php"); // redirect
-        }
+    if ($result) {
+        echo "<script>alert('New user Registered Successfuly')</script>";
+        echo "<script>window.open('login.php','_self')</script>";
     } else {
-        echo "<script>alert('Please enter your correct details')</script>";
+        echo "<script>alert('Sorry an error occured')</script>";
     }
-}
+} 
