92cdefd4b90f30d0a09b343f657a833043f7edd0
KARAN-SINGH19 <115944579+KARAN-SINGH19@users.noreply.github.com>
2023-11-27 23:56:06 +0400

    Update marking_stud_attendance_script.php

--- pages/marking_stud_attendance_script.php
+++ pages/marking_stud_attendance_script.php
@@ -11,26 +11,35 @@ if ($result > 0) {
         $id = $row['user_id'];
         $fname = $row['user_fname'];
         $lname = $row['user_lname'];
-        $email = $row['user_email'];
-        $role_id = $row['user_role_id'];
-        $course_id = $row['user_course_id'];
+       
 
         echo "<tr>
        
       <form method='post'>
-       <td><input type='hidden' name='id'  class='form-control' value='$id' readonly>$id</td> 
-       <td>$id</td>
-       <td>$fname</td>
-       <td>$lname</td>
-       <td>$email</td>
-       <td>$role_id</td>
-       <td>$course_id</td>
-      <td><input type='submit' name='present' class='btn btn-success' value='Present'>  </td> 
-      <td><input type='submit' name='absent' class='btn btn-danger' value='Absent'>  </td> 
-      <td><input type='submit' name='late' class='btn btn-secondary' value='Late'>  </td> 
+        <td>$fname $lname</td>
+        <td><input type='submit' name='present' class='btn btn-success' value='Present'>  </td> 
+        <td><input type='submit' name='absent' class='btn btn-danger' value='Absent'>  </td> 
+        <td><input type='submit' name='late' class='btn btn-secondary' value='Late'>  </td> 
      </form>
     </tr>";
     }
 } else {
     echo "<script> The table is empty </script>";
+}
+
+
+
+
+
+if (isset($_POST['present'])) {
+    
+}
+
+
+if (isset($_POST['absent'])) {
+    
+}
+
+if (isset($_POST['late'])) {
+ 
 }
