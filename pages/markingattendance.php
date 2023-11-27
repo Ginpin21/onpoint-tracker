6fceec8bf74aaba32ecf3bed1238966b8b2ab755
KARAN-SINGH19 <115944579+KARAN-SINGH19@users.noreply.github.com>
2023-11-27 23:20:56 +0400

    Update markingattendance.php

--- pages/markingattendance.php
+++ pages/markingattendance.php
@@ -25,7 +25,9 @@ if ($result > 0) {
        <td>$email</td>
        <td>$role_id</td>
        <td>$course_id</td>
-      <td><input type='submit' name='delete'  class='btn btn-danger' value='Remove'>  </td> 
+      <td><input type='submit' name='present' class='btn btn-success' value='Present'>  </td> 
+      <td><input type='submit' name='absent' class='btn btn-danger' value='Absent'>  </td> 
+      <td><input type='submit' name='late' class='btn btn-secondary' value='Late'>  </td> 
      </form>
     </tr>";
     }
