26766b51db6456a10964f8f9ddd12e7275dd0d4f
Ginpin21 <iftekharahmed2003@gmail.com>
2023-12-06 11:48:49 +0400

    Added login session logic to navbar and styled login apge

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,124 +1,76 @@
 <style>
-    .navbar {
-        padding: 10px;
-        position: sticky;
-    }
+.navbar {
+    padding: 10px;
+    position: sticky;
+}
 
-    .navbar-nav .active a {
-        font-weight: bold;
-    }
+.navbar-nav .active a {
+    font-weight: bold;
+}
 </style>
-
-
 <?php
 session_start();
-$_SESSION['role_id'];
-if ($_SESSION['role_id'] == 1) {
-    $currentPage = basename($_SERVER['PHP_SELF']);
-    echo "Current Page: $currentPage";
-
-    echo "
-    <nav class='navbar navbar-expand-lg navbar-light bg-light'>
-        <a class='navbar-brand' href='index.php'>Onpoint Tracker</a>
-        <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
-            <span class='navbar-toggler-icon'></span>
-        </button>
-        <div class='collapse navbar-collapse justify-content-end' id='navbarNav'>
-            <ul class='navbar-nav ml-auto'>
-                <li class='nav-item " . ($currentPage == 'index.php' ? 'active' : '') . "'>
-                    <a class='nav-link' href='index.php'>Home</a>
-                </li>
-                <li class='nav-item " . ($currentPage == 'admin_dashboard.php' ? 'active' : '') . "'>
-                    <a class='nav-link' href='admin_dashboard.php'>Admin Dashboard</a>
-                </li>
-            </ul>
-        </div>
-        <div class='d-flex align-items-center gap-3'>
-            <span class='navbar-text' style='color: #003DB2; font-weight: bold;'>
-                " . $_SESSION['first_name'] . "&nbsp;" . $_SESSION['last_name'] . "<br>" . $_SESSION['email'] . "
-            </span>
-            <form class='form-inline'>
-                <button class='btn btn-danger' type='submit'>Logout</button>
-            </form>
-        </div>
-    </nav>
-    ";
-}
+$currentPage = basename($_SERVER['PHP_SELF']);
 ?>
-
-
-<?php
-$_SESSION['role_id'];
-if ($_SESSION['role_id'] == 2) {
-    $currentPage = basename($_SERVER['PHP_SELF']);
-    echo "Current Page: $currentPage";
-
-    echo "
-    <nav class='navbar navbar-expand-lg navbar-light bg-light'>
-        <a class='navbar-brand' href='index.php'>Onpoint Tracker</a>
-        <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
-            <span class='navbar-toggler-icon'></span>
-        </button>
-        <div class='collapse navbar-collapse justify-content-end' id='navbarNav'>
-            <ul class='navbar-nav ml-auto'>
-                <li class='nav-item " . ($currentPage == 'index.php' ? 'active' : '') . "'>
-                    <a class='nav-link' href='index.php'>Home</a>
-                </li>
-                <li class='nav-item " . ($currentPage == 'student_dashboard.php' ? 'active' : '') . "'>
-                    <a class='nav-link' href='student_dashboard.php'>Student Dashboard</a>
-                </li>
-            </ul>
+<nav class='navbar navbar-expand-lg navbar-light bg-light'>
+    <a class='navbar-brand' href='index.php'>Onpoint Tracker</a>
+    <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav'
+        aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+        <span class='navbar-toggler-icon'></span>
+    </button>
+    <?php
+    ?>
+    <div class='collapse navbar-collapse justify-content-end' id='navbarNav'>
+        <ul class='navbar-nav ml-auto'>
+            <li class="<?php echo 'nav-item ' . ($currentPage == 'index.php' ? 'active' : '') ?>">
+                <a class='nav-link' href='index.php'>Home</a>
+            </li>
+            <?php if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"]) { ?>
+            <li class="<?php echo 'nav-item ' . (str_contains($currentPage, "_dashboard.php") ? 'active' : ''); ?>">
+                <?php
+                    $dashboard_link = "";
+                    switch ($_SESSION["role_name"]) {
+                        case "Administrator":
+                            $dashboard_link = "admin_dashboard.php";
+                            break;
+                        case "Teacher":
+                            $dashboard_link = "admin_dashboard.php";
+                            break;
+                        case "Student":
+                            $dashboard_link = "student_dashboard.php";
+                            break;
+                        default:
+                            $dashboard_link = "index.php";
+                            break;
+                    }
+                    ?>
+                <a class='nav-link' href="<?php echo $dashboard_link; ?>">Dashboard</a>
+            </li>
+            <?php
+            } else {
+            ?>
+            <li class="<?php echo 'nav-item ' . ($currentPage == 'login.php' ? 'active' : ''); ?>">
+                <a class='nav-link' href='login.php'>Login</a>
+            </li>
+            <?php } ?>
+        </ul>
+    </div>
+    <div class='d-flex align-items-center gap-4 px-4'>
+        <?php if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"]) { ?>
+        <div class="d-flex flex-column align-items-start" style="color: #003DB2; font-weight: bold;">
+            <?php
+                $user_fullname = $_SESSION["first_name"] . " " . $_SESSION["last_name"];
+                $user_role_name = $_SESSION["role_name"];
+                echo "<p style='margin:0'>$user_fullname</p>";
+                echo "<p style='margin:0'>$user_role_name</p>";
+                ?>
         </div>
-        <div class='d-flex align-items-center gap-3'>
-            <span class='navbar-text' style='color: #003DB2; font-weight: bold;'>
-                " . $_SESSION['first_name'] . "&nbsp;" . $_SESSION['last_name'] . "<br>" . $_SESSION['email'] . "
-            </span>
-            <form class='form-inline' action=''>
-                <button class='btn btn-danger' type='submit'>Logout</button>
-            </form>
-        </div>
-    </nav>
-    ";
-}
-?>
-
-
-<?php
-$_SESSION['role_id'];
-if ($_SESSION['role_id'] == 3) {
-    $currentPage = basename($_SERVER['PHP_SELF']);
-    echo "Current Page: $currentPage";
-
-    echo "
-    <nav class='navbar navbar-expand-lg navbar-light bg-light'>
-        <a class='navbar-brand' href='index.php'>Onpoint Tracker</a>
-        <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
-            <span class='navbar-toggler-icon'></span>
-        </button>
-        <div class='collapse navbar-collapse justify-content-end' id='navbarNav'>
-            <ul class='navbar-nav ml-auto'>
-                <li class='nav-item " . ($currentPage == 'index.php' ? 'active' : '') . "'>
-                    <a class='nav-link' href='index.php'>Home</a>
-                </li>
-                <li class='nav-item " . ($currentPage == 'teacher_dashboard.php' ? 'active' : '') . "'>
-                    <a class='nav-link' href='teacher_dashboard.php'>Teacher Dashboard</a>
-                </li>
-            </ul>
-        </div>
-        <div class='d-flex align-items-center gap-3'>
-            <span class='navbar-text' style='color: #003DB2; font-weight: bold;'>
-                " . $_SESSION['first_name'] . "&nbsp;" . $_SESSION['last_name'] . "<br>" . $_SESSION['email'] . "
-            </span>
-            <form class='form-inline'>
-                <button class='btn btn-danger' type='submit'>Logout</button>
-            </form>
-        </div>
-    </nav>
-    ";
-}
-?>
-
-
+        <form class='form-inline' action="logout.php">
+            <button class='btn btn-danger' type='submit'>Logout</button>
+        </form>
+        <?php } ?>
+    </div>
+</nav>
 
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
