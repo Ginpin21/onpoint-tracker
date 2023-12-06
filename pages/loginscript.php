cd3aa51c5bb7ffb4d662042e26d5a30614285c5d
KARAN-SINGH19 <115944579+KARAN-SINGH19@users.noreply.github.com>
2023-12-06 13:28:56 +0400

    updated the script for login

--- pages/loginscript.php
+++ pages/loginscript.php
@@ -4,7 +4,7 @@ require('../inc/connection.php');
 if (isset($_POST['Login'])) {
     $email = $_POST['email'];
     $pass = md5($_POST['pass']);
-    $sql = "SELECT * FROM user WHERE user_email = '$email' AND user_password = '$pass' ";
+    $sql="SELECT user.*, role.role_name from user JOIN role on user.user_role_id=role.role_id where user_email = '$email' AND user_password = '$pass';";
     $result = mysqli_query($conn, $sql);
     $final = mysqli_num_rows($result);
     if ($final > 0) {
@@ -13,11 +13,12 @@ if (isset($_POST['Login'])) {
             $_SESSION['first_name'] = $row['user_fname'];
             $_SESSION['last_name'] = $row['user_lname'];
             $_SESSION['email'] = $row['user_email'];
-            $_SESSION['role_id'] = $row['user_role_id'];
             $_SESSION['course_id'] = $row['user_course_id'];
-            if ($_SESSION['role_id'] == 1) {
+            $_SESSION['name']=$row['role_name'];
+            echo  $_SESSION['name'];
+            if ($_SESSION['name'] == 'Administrator') {
                 header("Location: admin_dashboard.php");
-            } elseif ($_SESSION['role_id'] == 2) {
+            } elseif ($_SESSION['name'] == 'Student') {
                 header("Location: student_dashboard.php");
             } else {
                 header("Location: teacher_dashboard.php");
